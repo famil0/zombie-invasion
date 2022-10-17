38a9a98fe00334eea8053acda623913d64120578
Fazekas Milán <fazekas.milan1@gmail.com>
2022-10-17 19:44:18 +0200

    switch to 2D map

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ let placeSize = 40;
 let col = 20;
 let row = 20;
 let map = [];
-let playerIdx;
+let playerLocation = {
+    x: 0,
+    y: 0
+};
+let flashlightIsOn = false;
+let flashlightPlaces = [];
 
 /**@type{HTMLDivElement}*/
 let mapContainer = document.querySelector(".map-container");
@@ -17,35 +22,40 @@ main();
 
 
 function main() {
-    mapContainer.style.setProperty("--col", col);
+    mapContainer.style.setProperty("--row", row);
     root.style.setProperty("--size", placeSize + "px");
     newMap();
 }
 
 function newMap() {
-    for (let i = 0; i < col * row; i++) {        
-        let d = document.createElement("div");
-        d.classList.add("place");
-        map.push(d);
-        mapContainer.appendChild(d);
+    for (let x = 0; x < col; x++) {
+        let arr = [];
+        for (let y = 0; y < row; y++) {
+            let d = document.createElement("div");
+            d.classList.add("place");
+            arr.push(d);
+            mapContainer.appendChild(d);
+        }
+        map.push(arr);
     }
-    let r = Math.floor(col*row*Math.random());
-    map[r].classList.add("player");
-    playerIdx = r;
+    let x = Math.floor(col*Math.random());
+    let y = Math.floor(row*Math.random());
+    map[x][y].classList.add("player");
+    playerLocation = {x, y};
 }
 
 function keyPress(e) {
-    if (e.key == 'w' && playerIdx >= col) {
-        moveFromTo(playerIdx, playerIdx - col);
+    if (e.key == 'w' && playerLocation.y > 0) {
+        moveTo(playerLocation.x, playerLocation.y - 1);
     }
-    else if (e.key == 's' && playerIdx < col * row - col) {
-        moveFromTo(playerIdx, playerIdx + col);
+    else if (e.key == 's' && playerLocation.y < row - 1) {
+        moveTo(playerLocation.x, playerLocation.y + 1);
     }
-    else if (e.key == 'd' && (playerIdx + 1) % col != 0) {
-        moveFromTo(playerIdx, playerIdx + 1);
+    else if (e.key == 'a' && playerLocation.x > 0) {
+        moveTo(playerLocation.x - 1, playerLocation.y);
     }
-    else if (e.key == 'a' && playerIdx % col != 0) {
-        moveFromTo(playerIdx, playerIdx - 1);
+    else if (e.key == 'd' && playerLocation.x < col - 1) {
+        moveTo(playerLocation.x + 1, playerLocation.y);
     }
 
     if (e.key == 'i') {
@@ -60,10 +70,40 @@ function keyPress(e) {
     else if (e.key == 'j') {
         root.style.setProperty("--heading", "270deg");
     }
+
+    if (e.key == " ") {        
+        removeFlashlights();
+        flashlightIsOn = !flashlightIsOn;
+        flashlight();        
+    }
 }
 
-function moveFromTo(from, to) {
-    map[to].classList.toggle("player");
-    map[from].classList.toggle("player");
-    playerIdx = to;
+function removeFlashlights() {
+    flashlightPlaces.forEach(p => {
+        p.classList.remove("flashlight");
+    });
+    flashlightPlaces = [];
+}
+
+function moveTo(x, y) {
+    map[playerLocation.x][playerLocation.y].classList.toggle("player");
+    map[x][y].classList.toggle("player");
+    playerLocation = {x, y};
+    removeFlashlights();
+    flashlight();
+}
+
+function flashlight() {
+    if (flashlightIsOn) {
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < i * 2 + 1; j++) {
+                try {
+                    let p = map[playerLocation.x + j - i][playerLocation.y - i - 1];
+                    p.classList.add("flashlight");
+                    flashlightPlaces.push(p);
+                }
+                catch {}
+            }
+        }
+    }
 }
