ec00b79faa74d89060da97fc03bca0518dc1e8aa
Fazekas Milán <fazekas.milan1@gmail.com>
2022-10-17 18:17:28 +0200

    keyboard movement

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let placeSize = 30;
 let col = 20;
 let row = 20;
 let map = [];
+let playerIdx;
 
 /**@type{HTMLDivElement}*/
 let mapContainer = document.querySelector(".map-container");
 /**@type{HTMLDivElement}*/
 let root = document.querySelector(":root");
+
+document.body.addEventListener("keypress", keyPress);
 
 
 
@@ -23,42 +26,32 @@ function newMap() {
     for (let i = 0; i < col * row; i++) {        
         let d = document.createElement("div");
         d.classList.add("place");
-        d.addEventListener("click", click);
         map.push(d);
         mapContainer.appendChild(d);
     }
-    map[Math.floor(col*row*Math.random())].classList.add("player");
+    let r = Math.floor(col*row*Math.random());
+    map[r].classList.add("player");
+    playerIdx = r;
 }
 
+function keyPress(e) {
 
-function click(e) {
-    let target = e.target;
-    let idx = 0;
-    for (let i = 0; i < map.length; i++) {
-        if (map[i] == target) {
-            idx = i;
-            break;
-        }
+    if (e.key == 'w') {
+        moveFromTo(playerIdx, playerIdx - col);
     }
-    if (map[idx - col].classList.contains("player")) {
-        console.log("move down");
-        moveTo(idx, idx - col);
+    else if (e.key == 's') {
+        moveFromTo(playerIdx, playerIdx + col);
     }
-    else if (map[idx + 1].classList.contains("player")) {
-        console.log("move left");
-        moveTo(idx, idx + 1);
+    else if (e.key == 'd') {
+        moveFromTo(playerIdx, playerIdx + 1);
     }
-    else if (map[idx - 1].classList.contains("player")) {
-        console.log("move right");
-        moveTo(idx, idx - 1);
-    }
-    else if (map[idx + col].classList.contains("player")) {
-        console.log("move up");
-        moveTo(idx, idx + col);
+    else if (e.key == 'a') {
+        moveFromTo(playerIdx, playerIdx - 1);
     }
 }
 
-function moveTo(from, to) {
+function moveFromTo(from, to) {
     map[to].classList.toggle("player");
     map[from].classList.toggle("player");
+    playerIdx = to;
 }
