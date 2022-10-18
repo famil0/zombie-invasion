76fc3b952f315411c6eadeacf840e596f77ea1dc
Fazekas Milán <fazekas.milan1@gmail.com>
2022-10-18 22:14:40 +0200

    add hit animation

--- entity.js
+++ entity.js
@@ -4,35 +4,42 @@ class Entity {
         this.y = pos.y;
         this.heading = heading;
         this.type = type;
-        this.d = document.createElement("div");
+        this.div = document.createElement("div");
+        this.div.addEventListener("webkitAnimationEnd", () => { this.div.classList.remove("hit"); })
+        this.health = 100;
     }
 
     show() {
-        this.d.classList.add(this.type);
-        map[this.x][this.y].appendChild(this.d);
+        this.div.classList.add(this.type);
+        map[this.x][this.y].appendChild(this.div);
     }
 
     move(dir) {
         if (map[this.x + dir.x][this.y + dir.y].children.length >= 1) {return};
-        map[this.x][this.y].removeChild(this.d)
+        map[this.x][this.y].removeChild(this.div)
         this.x += dir.x;
         this.y += dir.y;
-        map[this.x][this.y].appendChild(this.d);
+        map[this.x][this.y].appendChild(this.div);
     }
 
     rotate(dir) {
         this.heading = dir;
         if (dir == directions.up) {
-            this.d.id = "up";
+            this.div.id = "up";
         }
         else if (dir == directions.down) {
-            this.d.id = "down";
+            this.div.id = "down";
         }
         else if (dir == directions.right) {
-            this.d.id = "right";
+            this.div.id = "right";
         }
         else if (dir == directions.left) {
-            this.d.id = "left";
+            this.div.id = "left";
         }
+    }
+
+    hit(amount, target) {
+        target.health -= amount;
+        target.div.classList.add("hit");
     }
 }
